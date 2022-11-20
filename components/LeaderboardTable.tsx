992cada406eef8b3d955cb72d1206d491dd74874
Keith Anderson <kand198@aucklanduni.ac.nz>
2022-11-21 00:03:20 +1300

    Generate reports

--- components/LeaderboardTable.tsx
+++ components/LeaderboardTable.tsx
@@ -1,12 +1,13 @@
-import { ScrollArea, Space, Table } from '@mantine/core';
+import { Button, ScrollArea, Space, Table } from '@mantine/core';
 import { useCompetition } from '../lib/CompetitionProvider';
 import Team, { vehicleClassMap, vehicleTypeMap } from '../lib/TeamInterface';
 import { Filter, teamMeetsFilters } from '../lib/Filters';
 import { getTotalTime } from '../lib/TimeHelpers';
 import { getGymkhanaTimeFromRun } from './GymkhanaTable';
+import { stringify } from 'csv-stringify/sync';
 
 const LeaderboardTable = ({ sortBy, filters, hideTechnicalReport }: { sortBy?: string; filters?: Filter[]; hideTechnicalReport?: boolean }) => {
-  const { teams, laps } = useCompetition();
+  const { teams, laps, metadata } = useCompetition();
 
   const getDragPoints = (team: Team) => {
     const time = Math.min(...team.events.drag);
@@ -148,6 +149,93 @@ const LeaderboardTable = ({ sortBy, filters, hideTechnicalReport }: { sortBy?: s
     }
   });
 
+  const exportLeaderboard = () => {
+    const classTypeList = [
+      ...new Set(
+        sortedTeams?.map((team) => {
+          return `${team.class} ${team.type}`;
+        })
+      ),
+    ];
+
+    const classTypeScores = classTypeList.map((classType) => {
+      const classTypeTeams = sortedTeams?.filter((team) => {
+        return `${team.class} ${team.type}` === classType;
+      });
+      const teamScores = classTypeTeams.map((team) => ({
+        name: team.name,
+        school: team.school,
+        class: team.class,
+        type: team.type,
+        overall: getTotalPoints(team),
+        drag: getDragPoints(team),
+        gymkhana: getGymkhanaPoints(team),
+        endurance: getEndurancePoints(team),
+        efficiency: getEfficiencyPoints(team),
+      }));
+      const eventRankings = teamScores
+        .map((team) => ({
+          name: team.name,
+          school: team.school,
+          class: vehicleClassMap.get(team.class),
+          type: vehicleTypeMap.get(team.type),
+          overall:
+            teamScores
+              .map((team) => team.overall)
+              .sort((a, b) => b - a)
+              .indexOf(team.overall) + 1,
+          drag:
+            teamScores
+              .map((team) => team.drag)
+              .sort((a, b) => b - a)
+              .indexOf(team.drag) + 1,
+          gymkhana:
+            teamScores
+              .map((team) => team.gymkhana)
+              .sort((a, b) => b - a)
+              .indexOf(team.gymkhana) + 1,
+          endurance:
+            teamScores
+              .map((team) => team.endurance)
+              .sort((a, b) => b - a)
+              .indexOf(team.endurance) + 1,
+          efficiency:
+            teamScores
+              .map((team) => team.efficiency)
+              .sort((a, b) => b - a)
+              .indexOf(team.efficiency) + 1,
+        }))
+        .sort((a, b) => a.overall - b.overall);
+      return {
+        classType,
+        eventRankings,
+      };
+    });
+
+    const columns = ['name', 'school', 'class', 'type', 'overall', 'drag', 'gymkhana', 'endurance', 'efficiency'];
+
+    // Generate a separate CSV object for each class type
+    const csvs = classTypeScores.map((classTypeScore) => {
+      const { classType, eventRankings } = classTypeScore;
+      const csv = stringify(eventRankings, { header: true, columns });
+      return { classType, csv };
+    });
+
+    // Combine CSVs into one CSV with an empty line between each class type
+    const combinedCsv = csvs.reduce((acc, csv) => {
+      return acc + csv.csv + '\n\n';
+    }, '');
+
+    // Download the CSV
+    const blob = new Blob([combinedCsv], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `leaderboard-${metadata.name}.csv`);
+    document.body.appendChild(link);
+    link.click();
+  };
+
   const LeaderboardHeader = (
     <thead>
       <tr>
@@ -190,6 +278,9 @@ const LeaderboardTable = ({ sortBy, filters, hideTechnicalReport }: { sortBy?: s
   return (
     <>
       <ScrollArea className='relative'>
+        <Button type='button' className='bg-blue-600 hover:bg-blue-800' onClick={() => exportLeaderboard()}>
+          Download Report
+        </Button>
         <Table className='overflow-x-scroll whitespace-nowrap'>
           {LeaderboardHeader}
           {LeaderboardBody}
